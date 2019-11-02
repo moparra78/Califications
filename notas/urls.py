49609c4baefbf249ef3552630097fbc2edf5a268
Monica <moparra78@uan.edu.co>
2019-11-02 12:33:42 -0500

    arreglado

--- notas/urls.py
+++ notas/urls.py
@@ -15,25 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-##from django.conf.urls import url, include #11oct
-##from django.contrib.auth.models import User #11oct
-##from rest_framework import routers, serializers, viewsets #11oct
+from django.conf.urls import url, include #11oct
+from django.contrib.auth.models import User #11oct
+from rest_framework import routers, serializers, viewsets #11oct
 
-##from registro.views import FacultadViewSet
-
+from registro.views import FacultadViewSet
+from django.contrib import admin
+from django.urls import include, path
 # Routers provide an easy way of automatically determining the URL conf.
-##router = routers.DefaultRouter() #11oct
-##router.register(r'facultad', FacultadViewSet) #11oct
+router = routers.DefaultRouter() #11oct
+router.register(r'facultad', FacultadViewSet) #11oct
 
-##urlpatterns = [
-  ##  path('admin/', admin.site.urls),
-##]
-##from django.contrib import admin
-##from django.urls import include, path
-
-##urlpatterns = [
-  ##  path('registro/', include('registro.urls')),
-    ##path('admin/', admin.site.urls),
-    ##url(r'^', include(router.urls)), #11oct
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('registro/', include('registro.urls')),
+    url(r'^', include(router.urls)), #11oct
     #url(r'^api-auth/', include('rest_framework.urls', namespace'rest_framework')), #11oct
 ]
--- registro/urls.py
+++ registro/urls.py
@@ -6,23 +6,6 @@ from rest_framework import routers, serializers, viewsets #11oct
 from . import views
 from registro.views import FacultadViewSet #11oct
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter() #11oct
-router.register(r'facultad', FacultadViewSet) #11oct
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path('registro/', include('registro.urls')),
-    path('admin/', admin.site.urls),
-    url(r'^', include(router.urls)), #11oct
-    url(r'^api-auth/', include('rest_framework.urls', namespace'rest_framework')) #11oct
-]
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
